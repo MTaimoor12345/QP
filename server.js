9f07293b26e2cdd31b31cbdd49c4792bc0a32fdf
MTaimoor12345 <125217656+MTaimoor12345@users.noreply.github.com>
2024-04-16 17:58:20 +0500

    oik

--- server.js
+++ server.js
@@ -5,25 +5,20 @@ const path = require('path');
 const expressLayout=require('express-ejs-layouts')
 const PORT =process.env.PORT || 3300
 
-app.use(express.static('public'));
-app.get('/',(req,res)=>{
-   res.render("home")
-});
-app.get('/cart',(req,res)=>{
-    res.render("customer/cart")
- });
- app.get('/login',(req,res)=>{
-    res.render("auth/login")
- });
- app.get('/register',(req,res)=>{
-    res.render("auth/register")
- });
 
-//set Tempelate Engine
+
+
+app.use(express.static('public'));
+
 
 app.use(expressLayout);
 app.set('views',path.join(__dirname,'/resources/views'));
 app.set('view engine','ejs');
+require('./routes/web')(app);
+
+//set Tempelate Engine
+
+
 
 app.listen(PORT,()=>{
     console.log(`listen on Port ${PORT}`)
