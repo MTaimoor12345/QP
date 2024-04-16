4aca29c7ca8e4c1d0dbb424e1a7d361190603210
MTaimoor12345 <125217656+MTaimoor12345@users.noreply.github.com>
2024-04-16 10:31:26 +0500

    ok

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
    res.render("home")
 });
+app.get('/cart',(req,res)=>{
+    res.render("customer/cart")
+ });
+ app.get('/login',(req,res)=>{
+    res.render("auth/login")
+ });
+ app.get('/register',(req,res)=>{
+    res.render("auth/register")
+ });
 
 //set Tempelate Engine
 
